b3ef539466c2e8c494c138800aabd04df1e7a692
silveiraluisf <silveiraluis88@gmail.com>
2022-09-22 14:58:22 -0300

    criado o widget de texto separado do main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_flutter_treino_4/question.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = ['Qual a sua cor favorita?', 'Qual o seu animal favorito?'];
@@ -21,13 +34,13 @@ class MyApp extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Text('Essa é uma pergunta?',
-                    style: TextStyle(fontSize: 24)),
+                child: Question(questions[_questionIndex]),
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: ElevatedButton(
                   onPressed: () {
+                    answerQuestion();
                     print('Resposta 1 escolhida!');
                   },
                   child: Text('Resposta 1'),
